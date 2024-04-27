4478ca202500e9fda90210d4f86fe2ce39d4dc84
tarik <lonetariq514@gmail.com>
2024-04-27 14:56:16 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+...
+...
+
+color = "green"
+
+@app.route("/")
+def main():
+    print(color)
+    return render_template('hello.html', color=color)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8001")
